31f7553e613be659822d31dfb1692a05898891fa
Gabriel Costa <gbrlcst148@gmail.com>
2021-10-12 00:51:11 -0300

    Adicionando enunciados dos exercícios

--- src/Exercicio1.java
+++ src/Exercicio1.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 public class Exercicio1 {
 
 	public static void main(String[] args) {
+		/* Para um ano de nascimento fornecido pelo usuário, informe a idade que ele terá no
+		dia 31 de dezembro de 2021. */
+		
 		Scanner leitor = new Scanner(System.in);
 		int anoDeNascimento;
 		System.out.print("Digite seu ano de nascimento: ");
--- src/Exercicio2.java
+++ src/Exercicio2.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 public class Exercicio2 {
 
 	public static void main(String[] args) {
+		/* Sabendo que cada hora dura 60 minutos e que cada minuto dura 60 segundos,
+		solicite ao usuário que digite o tempo de um filme em minutos e informe a duração
+		desse filme em horas e em segundos. */
+		
 		Scanner leitor = new Scanner(System.in);
 		int tempoEmMinutos;
 		
